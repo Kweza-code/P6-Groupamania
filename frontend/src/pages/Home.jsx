47f509886ebac7c948427c06f035dc57c2a1ee99
innocent75 <99754303+innocent75@users.noreply.github.com>
2022-10-15 11:33:49 +0200

    Pour mentor

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,37 +5,37 @@ import { useState } from "react";
 import { Form } from "react-router-dom";
 
 const Home = () => {
-  const [post, setPost] = useState([]);
-
-  fetch("http://localhost:3000/api/posts", {
-    method: "GET",
-  })
-    .then((res) => {
-      setPost(res.data[0]);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  //
-  {
-    Form.map((post) => {
-      return (
-        <div>
-          <section class="posts">
-            <Post
-              date={props.date}
-              description={props.description}
-              author={props.author}
-              title={props.title}
-              dislike={props.dislike}
-              like={props.like}
-            />
-          </section>
-          <Footer />
-        </div>
-      );
-    });
-  }
+  return (
+    <div>
+      <section class="posts">
+        <Post
+          date="16/02/2022 à 16h23"
+          description="Salut, hier je suis parti manger un burger"
+          author="@Adrien"
+          title="Le burger"
+          dislike="32"
+          like="4"
+        />
+        <Post
+          date="16/02/2022 à 16h25"
+          description="Salut, hier je suis parti manger à la cantine"
+          author="@Julien"
+          title="La cantine"
+          dislike="34"
+          like="42"
+        />
+        <Post
+          date="16/02/2022 à 18h"
+          description="Salut, je suis parti aux toilettes"
+          author="@Tom"
+          title="Les toilettes"
+          dislike="35"
+          like="41"
+        />
+      </section>
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
