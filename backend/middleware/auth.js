4ead530987a81d01764dc0ad5599f022c6e640fc
innocent75 <99754303+innocent75@users.noreply.github.com>
2022-10-16 16:29:43 +0200

    Tentative connextion

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,7 +2,6 @@ const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv").config();
 
 module.exports = (req, res, next) => {
-  const { email, password } = req.body;
   try {
     const token = req.headers.authorization.split(" ")[1];
     const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,18 +6,37 @@ import Create from "./pages/Create";
 import Home from "./pages/Home";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
+import { UidContext } from "./components/AppContext";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
+  const [uid, setUid] = useState(null);
+  useEffect(() => {
+    const fetchToken = async () => {
+      await axios({
+        method: "get",
+        url: `${process.env.REACT_APP_API_URL}jwt`,
+        withCredentials: true,
+      })
+        .then((res) => setUid(res.data))
+        .catch((err) => console.log("No token"));
+    };
+    fetchToken();
+  }, []);
+
   return (
     <BrowserRouter>
       <Navigation />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/*" element={<Home />} />
-        <Route path="create" element={<Create />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
+      <UidContext.Provider value={uid}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/*" element={<Home />} />
+          <Route path="create" element={<Create />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </UidContext.Provider>
     </BrowserRouter>
   );
 };
