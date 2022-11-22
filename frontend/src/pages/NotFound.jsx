5287783f9dd72b8a5785a725ff7371baea6ad45c
innocent75 <99754303+innocent75@users.noreply.github.com>
2022-11-22 23:25:19 +0100

    Fin du project Groupamania

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,11 +1,10 @@
 const NotFound = () => {
-
-	return (
-		<div>
-			<h1>404</h1>
-			<div className="inscription__inputs">Page not found</div>
-		</div>
-	);
+  return (
+    <div className="pagenotfound">
+      <h1>404</h1>
+      <div className="pagenotfound__inputs">Page not found</div>
+    </div>
+  );
 };
 
 export default NotFound;
